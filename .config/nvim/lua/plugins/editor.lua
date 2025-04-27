e31104a24523b682356fa1447ba08b3e6f8f4baa
dngtnv <kakalupin@gmail.com>
2025-04-27 10:43:36 +0700

    update config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,13 +1,15 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    pickers = {
-      find_files = {
-        file_ignore_patterns = { "node_modules", "build", "dist", ".next" },
-      },
-      live_grep = {
-        file_ignore_patterns = { "node_modules", "build", "dist", ".next" },
-      },
-    },
-  },
+	"nvim-telescope/telescope.nvim",
+	opts = {
+		pickers = {
+			find_files = {
+				hidden = true,
+				file_ignore_patterns = { "node_modules", "build", "dist", ".next" },
+			},
+			live_grep = {
+				hidden = true,
+				file_ignore_patterns = { "node_modules", "build", "dist", ".next" },
+			},
+		},
+	},
 }
