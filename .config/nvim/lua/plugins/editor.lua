68cc9ce0c209cdd4cd67e98ef3f17cb1ab04b363
dngtnv <kakalupin@gmail.com>
2025-06-19 09:42:56 +0700

    remove unnecessary plugins

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,15 +1,1 @@
-return {
-	"nvim-telescope/telescope.nvim",
-	opts = {
-		pickers = {
-			find_files = {
-				hidden = true,
-				file_ignore_patterns = { "node_modules", "build", "dist", ".next" },
-			},
-			live_grep = {
-				hidden = true,
-				file_ignore_patterns = { "node_modules", "build", "dist", ".next" },
-			},
-		},
-	},
-}
+return {}
